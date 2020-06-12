ec2005b5b7d0bf46c8e93289f005850cbdd9d70a
maheknangia <63040474+maheknangia@users.noreply.github.com>
2020-06-12 21:43:00 +1200

    added code for basic image slider

--- Technology/app/src/main/java/com/example/technology/DetailActivity.java
+++ Technology/app/src/main/java/com/example/technology/DetailActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.ViewFlipper;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -13,11 +15,23 @@ public class DetailActivity extends AppCompatActivity {
     private ImageView Cover;
     private TextView title, price, description;
     String setPrice;
+    ViewFlipper imageSlider;
+    Button next;
+    Button back;
+    int images[] = {R.drawable.phone1, R.drawable.phone2, R.drawable.phone3};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        imageSlider = findViewById(R.id.imageSlider);
+        next = (Button) findViewById(R.id.left);
+        back = (Button) findViewById(R.id.right);
+
+        for (int image:images){
+            imageSlider(image);
+        }
 
         ImageView Cover =  findViewById(R.id.ProductImage);
         TextView title = findViewById(R.id.titleProduct);
@@ -33,6 +47,17 @@ public class DetailActivity extends AppCompatActivity {
         setPrice = "$ " + setPrice;
         price.setText(setPrice);
         Cover.setImageResource(details.getIntExtra("image", 0));
+    }
+
+    public void imageSlider (int image){
+        ImageView imageView = new ImageView(this);
+        imageView.setBackgroundResource(image);
+        imageSlider.addView(imageView);
+        imageSlider.setFlipInterval(3000); //3 seconds
+        imageSlider.setAutoStart(true);
+        //animation
+        imageSlider.setInAnimation(this, android.R.anim.slide_in_left);
+        imageSlider.setOutAnimation(this, android.R.anim.slide_out_right);
     }
 
 
